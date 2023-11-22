cb69f4e1fa4f43127380082692ed318b2f43cf98
Kimmo Niemi <46970027+Kimsi1@users.noreply.github.com>
2023-11-22 22:15:09 +0200

    Update routes.js

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -2,56 +2,90 @@ import express from 'express';
 import { Contacts, CustomerContacts, Customers } from './models.js';
 import { NotFound, NotImplemented } from './errorHandler.js';
 
-const routes = express.Router()
+// Create an Express router
+const routes = express.Router();
 
+// Ping route for testing
 routes.get('/ping', (_req, res) => {
-  return res.send({ message: 'pong' })
-})
+  return res.send({ message: 'pong' });
+});
 
-// Customers
+// Customers routes
+
+// Get all customers
 routes.get('/api/customers', async (_req, res) => {
-  const customers = await Customers.getAll()
-  return res.send(customers)
-})
+  const customers = await Customers.getAll();
+  return res.send(customers);
+});
 
+// Get a specific customer by ID
 routes.get('/api/customers/:customerId', async (req, res) => {
-  const { customerId } = req.params
-  const customer = await Customers.get(customerId)
-  if(!customer) {
-    throw new NotFound('Customer Not Found')
+  const { customerId } = req.params;
+  const customer = await Customers.get(customerId);
+  if (!customer) {
+    // Throw a NotFound error if the customer is not found
+    throw new NotFound('Customer Not Found');
   }
-  return res.send(customer)
-})
+  return res.send(customer);
+});
 
+// Add a new customer
 routes.post('/api/customers', async (req, res) => {
-  const customers = await Customers.add(req.body)
-  return res.send(customers)
-})
+  const customers = await Customers.add(req.body);
+  return res.send(customers);
+});
 
 // MB-TODO: Create route for updating customer
 routes.put('/api/customers/:customerId', async (req, res) => {
-  throw new NotImplemented()
-})
+  try {
+    // Extract customer ID from the request parameters
+    const { customerId } = req.params;
 
-// Contacts
-routes.get('/api/contacts', async (_req, res) => {
-  const contacts = await Contacts.getAll()
-  return res.send(contacts)
-})
+    // Get the existing customer data
+    const existingCustomer = await Customers.get(customerId);
 
-routes.get('/api/contacts/:contactId', async (req, res) => {
-  const { contactId } = req.params
-  const contact = await Contacts.get(contactId)
-  if(!contact) {
-    throw new NotFound('Contact not found')
+    // If the customer doesn't exist, throw a NotFound error
+    if (!existingCustomer) {
+      throw new NotFound('Customer Not Found');
+    }
+
+    // Update the existing customer with the new data from the request body
+    const updatedCustomer = await Customers.update(customerId, req.body);
+
+    // Return the updated customer data in the response
+    return res.send(updatedCustomer);
+  } catch (error) {
+    // Handle errors and send an appropriate response
+    return res.status(500).send({ error: 'Internal Server Error' });
   }
-  return res.send(contact)
-})
+});
 
+// Contacts routes
+
+// Get all contacts
+routes.get('/api/contacts', async (_req, res) => {
+  const contacts = await Contacts.getAll();
+  return res.send(contacts);
+});
+
+// Get a specific contact by ID
+routes.get('/api/contacts/:contactId', async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await Contacts.get(contactId);
+  if (!contact) {
+    // Throw a NotFound error if the contact is not found
+    throw new NotFound('Contact not found');
+  }
+  return res.send(contact);
+});
+
+// Add a new contact
 routes.post('/api/contacts', async (req, res) => {
-  const contacts = await Contacts.add(req.body)
-  return res.send(contacts)
-})
+  const contacts = await Contacts.add(req.body);
+  return res.send(contacts);
+});
+
+
 
 // MB-TODO: Create route for fetching contacts of a customer `/api/customers/:customerId/contacts`
 routes.get('/api/customers/:customerId/contacts', async (req, res) => {
@@ -68,4 +102,5 @@ routes.delete('/api/customers/:customerId/contacts/:contactId', async (req, res)
   throw new NotImplemented()
 })
 
+// Export the Express router
 export default routes
